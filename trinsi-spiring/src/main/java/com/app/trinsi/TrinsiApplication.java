dc5fb999b7d8c2af4541050f5a88fa79ce005b33
MicaTravica <40805992+MicaTravica@users.noreply.github.com>
2020-05-06 21:20:09 +0200

    kjar

--- trinsi-spiring/src/main/java/com/app/trinsi/TrinsiApplication.java
+++ trinsi-spiring/src/main/java/com/app/trinsi/TrinsiApplication.java
@@ -1,7 +1,12 @@
 package com.app.trinsi;
 
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieScanner;
+import org.kie.api.runtime.KieContainer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class TrinsiApplication {
@@ -10,4 +15,13 @@ public class TrinsiApplication {
         SpringApplication.run(TrinsiApplication.class, args);
     }
 
+    @Bean
+    public KieContainer kieContainer() {
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks
+                .newKieContainer(ks.newReleaseId("com.app.trinsi", "trinsi-kjar", "1.0.0-SNAPSHOT"));
+        KieScanner kScanner = ks.newKieScanner(kContainer);
+        kScanner.start(10_000);
+        return kContainer;
+    }
 }
